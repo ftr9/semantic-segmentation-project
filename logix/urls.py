4434cc265e2dfec7bb88807708037efa486035ca
= <=>
2024-01-09 17:36:48 +0545

    Feat: Added static file paths to serve static file

--- logix/urls.py
+++ logix/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import LogixApiView,LogixApiViewId
+from .views import LogixApiView,LogixApiViewId,FileUploadView
 
 urlpatterns = [
     path('api/',LogixApiView.as_view()),
-    path('api/<int:logixId>',LogixApiViewId.as_view())
+    path('api/<int:logixId>',LogixApiViewId.as_view()),
+    path('api/upload/',FileUploadView.as_view())
 ]
