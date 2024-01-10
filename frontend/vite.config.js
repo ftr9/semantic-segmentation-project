90358f6e6c4feea8ee1abd5b8e08665b31f6afb1
= <=>
2024-01-10 18:58:54 +0545

    CORS: Added backend endpoint

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/logix/api/upload': 'http://localhost:8000',
+      '/logix/api/?format=json': 'http://localhost:8000',
+      '/media': 'http://localhost:8000',
+    },
+  },
+});
